c62e346e390c8122c049720346b09e8341126837
Akshay Gulhane <argulhane10246@gmail.com>
2023-03-29 23:33:53 +0530

    commit2903231

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Tooltip from "./components/tooltip";
+import "./index";
 
-function App() {
+const TooltipTarget = styled.span``;
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Tooltip text="Thanks for hovering! i'm a tooltip" position="left" background="393e46">
+        <TooltipTarget>Hover over me</TooltipTarget>
+      </Tooltip>
+      
     </div>
   );
 }
-
-export default App;
